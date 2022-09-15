f88415400ee822899a037c93ff61fe3e76eef50b
arthaliatta <113326003+arthaliatta@users.noreply.github.com>
2022-09-15 15:26:14 +0700

    Create login.dart

---
+++ lib/login.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+    const Login({Key? key}) : super(key: key);
+    @override
+    Widget build(BuildContext context) {
+        return const Scaffold(
+            body: MyStatefulWidget(),
+        );
+    }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+    const MyStatefulWidget({Key? key}) : super(key: key);
+
+    @override
+    State createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
+    @override
+    Widget build(BuildContext context) {
+        return Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+                children: [
+                    Container(
+                        alignment: Alignment.center,
+                        //padding: const EdgeInsets.all(10),
+                        child: 
+                        Image.asset("image/logo.png",
+                        fit: BoxFit.contain,
+                        width: 200,
+                        ),
+                        ),
+                    Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                            'Log in',
+                            style: TextStyle(fontSize: 20),
+                        )),
+                    Container(
+                        padding: const EdgeInsets.all(10),
+                        child: TextField(
+                            controller: nameController,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(15),),
+                                ),
+                                labelText: 'Email',
+                            ),
+                        ),
+                    ),
+                    Container(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: TextField(
+                            obscureText: true,
+                            controller: passwordController,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(15),),
+                                ),
+                                labelText: 'Password',
+                            ),
+                        ),
+                    ),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                            //forgot password screen
+                        },
+                        child: const Text(
+                          'Forgot Password',
+                          style: TextStyle(color: Color.fromARGB(255, 13, 66, 255)),
+                          ),
+                            ),
+                    ),
+                            
+                    Container(
+                        decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 47, 175),
+                        borderRadius: BorderRadius.circular(10),
+                        ),
+                        height: 50,
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: TextButton(
+                            child: const Text(
+                                'Login',
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 255, 255, 255)
+                                    ),
+                  
+                            ),
+                            onPressed: () {
+                            },
+                        )
+                    ),
+                    Row(
+                        // ignore: sort_child_properties_last
+                        children: [
+                            //const Text('Does not have account?'),
+                            TextButton(
+                                child: const Text(
+                                    'Sign in',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color.fromARGB(255, 255, 47, 175),
+                                    ),
+                                ),
+                                
+                                onPressed: () {
+                                    //signup screen
+                                },
+                            )
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                ],
+            )
+        );
+    }
+}
