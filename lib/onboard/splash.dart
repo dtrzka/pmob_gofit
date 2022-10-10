d55559873341a75d57d77acb24f53856c620bfcf
dtrzka <102124897+dtrzka@users.noreply.github.com>
2022-10-10 21:57:57 +0700

    Update splash.dart

--- lib/onboard/splash.dart
+++ lib/onboard/splash.dart
@@ -17,7 +17,7 @@ class _SplashState extends State<Splash> {
   }
 
   _navigatetohome() async {
-    await Future.delayed(Duration(milliseconds: 2000), () {});
+    await Future.delayed(Duration(milliseconds: 10000), () {});
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => Cover()));
   }
