2f01a71cf7c97ccc5be66dbe23f69e2645f44116
arthaliatta <113326003+arthaliatta@users.noreply.github.com>
2022-11-06 21:11:21 +0700

    step gif

--- lib/gifstep.dart
+++ lib/gifstep.dart
@@ -7,16 +7,63 @@ class Steps extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var title = 'Web Images';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body:
-      ),
-    );
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              //padding: const EdgeInsets.all(10),
+              child: Image.asset(
+                "images/step 1.gif",
+                fit: BoxFit.contain,
+                //width: 200,
+              ),
+            ),
+            const SizedBox(
+                height: 20,
+              ),
+            Text(
+              //"Stand with your feet together and your hands at your sides",
+              "Step Jack",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+                fontSize: 18,
+                
+                ),
+                ),
+            const SizedBox(
+                height: 20,
+              ),
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('Stand with your feet together and your hands at your sides'),
+                ),
+                ),
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('Take a lateral step and clap your hands above your head at the same time'),
+                ),
+                ),
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('Return to the starting position'),
+                ),
+                ),
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('Repeat'),
+                ),
+                ),
+             ],
+             
+        )
+        ));
   }
-}
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_1/gifstep.dart';
 import 'package:flutter_1/widget/column_Cards.dart';
 import 'package:flutter_1/onboard/splash.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // title: 'GoFit',
-      home: Splash(),
+      home: Steps(),
     );
   }
 }
