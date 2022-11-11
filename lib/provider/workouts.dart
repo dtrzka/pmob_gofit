0f6910c3685bda4f37d207b452b4f4b98ee6c304
dtrzka <102124897+dtrzka@users.noreply.github.com>
2022-11-11 19:51:35 +0700

    Create workouts.dart

---
+++ lib/provider/workouts.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_1/models/workout.dart';
+
+import '../models/workout.dart';
+
+class Workout with ChangeNotifier {
+  List<Workouts> _allWo = [
+    Workouts(
+        id: "1",
+        title: "Full Body Exercise",
+        text: "20 Minutes | Beginner",
+        image: 'full.jpg'),
+    Workouts(
+        id: "2",
+        title: "Intense Abs Exercise",
+        text: "10 Minutes | Beginner",
+        image: 'abs.jpg'),
+    Workouts(
+        id: "3",
+        title: "Tone Your Arms Exercise",
+        text: "15 Minutes | Intermediate",
+        image: 'arms.jpg'),
+    Workouts(
+        id: "4",
+        title: "Yoga Women Exercise",
+        text: "8 Minutes | Intermediate",
+        image: 'yoga2.jpg'),
+  ];
+
+  List<Workouts> get allproducts {
+    return [..._allWo];
+  }
+
+  Workouts findById(workoutId) {
+    return _allWo.firstWhere((woId) => woId.id == workoutId);
+  }
+
+  // void addProduct() {
+  //   _allproducts.add(value);
+  //   notifyListeners();
+  // }
+}
