20abbad089f830576a0d4bfed77881ce15c1f854
dtrzka <102124897+dtrzka@users.noreply.github.com>
2022-11-11 19:51:39 +0700

    Create workout.dart

---
+++ lib/models/workout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Workouts {
+  final String id;
+  final String title;
+  final String text;
+  final String image;
+
+  Workouts({
+    required this.id,
+    required this.title,
+    required this.text,
+    required this.image,
+  });
+}
