83e9dd75bea0e792b5d80444ed37df264730c8b0
dtrzka <102124897+dtrzka@users.noreply.github.com>
2022-12-17 12:47:53 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_1/provider/workouts.dart';
 import 'package:flutter_1/onboard/splash.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
